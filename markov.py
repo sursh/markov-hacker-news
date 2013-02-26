cc365e24fe65259a2f18aff069973bf1147d6beb
Sasha Laundy <sasha.laundy@gmail.com>
2013-02-26 16:30:33 -0500

    Fix newline bug

--- markov.py
+++ markov.py
@@ -3,6 +3,7 @@
 import sys
 import json
 import re
+import collections
 
 class Markov(object):
   """docstring for ClassName"""
@@ -16,27 +17,49 @@ class Markov(object):
 
     headlines = map(unicode.lower, headlines) # TODO: keep proper nouns
     headlines = map(unicode.strip, headlines)
-    headlines = [re.split('\W+', headline) for headline in headlines]
+    headlines = [re.split('\W+', headline) for headline in headlines] # TODO: fix regex
+    for headline in headlines:
+      if headline[-1] == '': del(headline[-1]) # EMBRACE THE JANK
+
     headlines = [['^'] + headline + ['$'] for headline in headlines]
 
     return headlines
 
   def generateBigrams(self, tokens):
-
+    grams = []
+    for idx, item in enumerate(tokens[:-1]):
+      grams.append((item, tokens[idx+1]))
+    return grams
 
   def generateMatrix(self, filename):
-    
-    headlines = self.read(filename)
-
-    for headline in headlines
-
     # read in headlines, generate tuples
 
+    # initializes a 2D dictionary with default values of 0
+    self.matrix = {} 
+
+    headlines = self.read(filename)
+
+    for headline in headlines:
+      bigrams = self.generateBigrams(headline)
+      self.updateMatrix(bigrams)
+
+
+  def updateMatrix(self, bigrams):
+    '''Take a list of (w1, w2) bigrams
+    and updates the counts stored of count of word2 following word1
+    '''
+    for prev_word, current_word in bigrams:
+      self.matrix.setdefault(prev_word, collections.defaultdict(int))
+      
+      self.matrix[prev_word][current_word] += 1
+
+      #print '%r %r' % (prev_word, current_word)
 
   def generateText(self):
     pass
 
-
+  def __str__(self):
+    return str(self.matrix)
 
 def main():
 
